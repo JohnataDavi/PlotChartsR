ee0aa0c0b45d6b6a8fa507bc05c4ef2d644485f3
Johnata Davi <infojohnata@gmail.com>
2019-08-09 22:19:27 -0300

    0.0.1

--- Plot.R
+++ Plot.R
@@ -1,2 +1,25 @@
-# Plot data
-data = read.csv("tips.csv")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("readr")
+library(ggplot2)
+#library(dplyr)
+library(readr)
+library(magrittr)
+
+imdb <- readr::read_rds("data/imdb.rds")
+
+ggplot(data = imdb)
+
+
+my_data <- imdb %>%
+  mutate(
+    lucro = receita - orcamento,
+    lucro = ifelse(lucro <= 0, "Não", "Sim")
+  )  %>%
+  filter(!is.na(lucro)) %>% 
+  ggplot() +
+  geom_point(mapping = aes(x = orcamento, y = receita, color = lucro)) +
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  labs(title="Filmes IMDB", x = "Orçamento", y = "Arrecadação", color = "Houve lucro?") 
+
+plot(my_data)
