655115ba32bef93631d5421f6532ecfc5f58f5cf
Johnata Davi <infojohnata@gmail.com>
2019-08-17 20:43:49 -0300

    0.0.2

--- Plot.R
+++ Plot.R
@@ -1,23 +1,33 @@
 #install.packages("ggplot2")
 #install.packages("magrittr")
-#install.packages("readr")
+#install.packages("xlsx")
+
+# Para importar o aquivo com os dados do exel
+library(xlsx)
+# Para a formatação do gráfico 
 library(ggplot2)
-library(readr)
+# Para o operador pipe  %>%
 library(magrittr)
 
-imdb <- readr::read_rds("data/imdb.rds")
+# Importando o arquivo com os dados de filmes do IMDb
+data_imdb <- read.xlsx("data/data_imdb.xlsx", 1)
 
-ggplot(data = imdb)
+# Inicializando um objeto ggplot
+ggplot(data = data_imdb)
 
-my_data <- imdb %>%
+my_data <- data_imdb %>%
+  # Utilizando o mutate para criar a variável de lucro,
+  # este que é a coluna de receita menos a de orçamento
   mutate(
     lucro = receita - orcamento,
     lucro = ifelse(lucro <= 0, "Não", "Sim")
   )  %>%
+  # Se o lucro não for nulo
   filter(!is.na(lucro)) %>% 
   ggplot() +
   geom_point(mapping = aes(x = orcamento, y = receita, color = lucro)) +
-  geom_abline(intercept = 0, slope = 1, color = "red") +
-  labs(title="Filmes IMDB", x = "Orçamento", y = "Arrecadação", color = "Houve lucro?") 
+  geom_abline(intercept = 0, slope = 1, color = "black") +
+  labs(title="Filmes - IMDb", x = "Orçamento", y = "Arrecadação", color = "Houve lucro?") 
 
+# Plotando o gráfico final
 plot(my_data)
